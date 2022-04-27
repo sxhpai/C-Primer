559038f7b4cab27ac5eb24a26fda7d2885ef2df5
Sxhpai <63643109+sxhpai@users.noreply.github.com>
2022-04-27 11:50:05 +0800

    Add files via upload

---
+++ Chapter 1/test_1.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+
+//1.3
+
+// int main()
+// {
+//     //1.3
+//     std::cout<<"hello world!"<<std::endl;
+
+//     //1.4
+//     std::cout << "Enter two number:" << std::endl;
+//     int v1 = 0, v2 = 0;
+//     // std::cin >> v1 >> v2;
+//     std::cout << "The product of " << v1 << " and "<< v2 << " is " << v1 * v2 <<std::endl;
+
+//     //1.5
+//     std::cout << " sun " << v1 << std::endl;
+//     std::cout << " sun " << std::endl;
+//     std::cout << " " << std::endl;
+//     std::cout << v1 << std::endl;
+
+//     //1.6
+//     std::cout << " The sum of " << v1 << std::endl;
+//             //   << " and " << v2;  不合法
+
+//     //1.7
+//     // /*
+//     //  *
+//     //  */
+
+//     //1.8
+//     std::cout << "/*" << std::endl;
+//     std::cout << "*/" << std::endl;
+//     // std::cout << /* "*/" */;
+//     std::cout << /* "*/" /* "/* */ << std::endl;
+ 
+//     return 0;
+// }
+
+// 1.4.1
+
+// int main()
+// {
+//     // int i = 1, sum = 0;
+//     // while(i <= 10) {
+//     //     sum += i;
+//     //     i++;
+//     // }
+//     // std::cout <<" sum is : " <<sum << std::endl;
+
+//     //1.9
+//     // int i = 50, sum = 0; 
+//     // while (i <= 100) {
+//     //     sum += i;
+//     //     i++;
+//     // }
+//     // std::cout << " The sum of 50 to 100 is : " << sum << std::endl;
+
+//     //1.10
+//     // int i = 11;
+//     // while(i--) {
+//     //     std::cout << " " << i << std::endl;
+//     // }
+
+//     //1.11
+//     // int a = 0, b = 0;
+//     // std::cout << " Please input a and b : " << std::endl;
+//     // std::cin >> a >> b;
+//     // while(a <= b) {
+//     //     std::cout << a << std::endl;
+//     //     a++;
+//     // }
+
+//     return 0;
+// }
+
+//1.4.2
+
+// int main()
+// {
+//     // int sum = 0;
+//     // //从1 加到 10
+//     // for (int val = 1; val <= 10; val++) {
+//     //     sum += val;
+//     // }
+
+//     // std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+
+//     //1.12
+//     // int sum = 0;
+//     // for(int i = -100; i <= 100; i++) {
+//     //     sum += i;
+//     // }
+//     // std::cout << " sum is : " << sum << std::endl;
+
+//     //1.13
+//     //(1)
+//     // int sum = 0;
+//     // for(int i = 50; i <=100; i++) {
+//     //     sum += i;
+//     // }
+//     // std::cout << " sum is : " << sum << std::endl;
+//     // //(2)
+//     // for(int i = 10; i >= 0; i--) {
+//     //     std::cout << i << std::endl;
+//     // }
+//     // //(3)
+//     // int a = 0, b = 0;
+//     // std::cout << "P input a and b" << std::endl;
+//     // std::cin >> a >> b;
+//     // for(int i = a; i <= b; i++) {
+//     //     std::cout << i << std::endl;
+//     // }
+
+//     //1.15
+//     //语法错误
+//     // std::cout << "Read each file." << std::endl:
+
+//     // std::cout << sun << std::endl;
+
+//     // std::cout << "sun" std::endl;
+
+//     // return 0
+//     //编辑-编译-调试周期
+
+//     // int sum = 0, value = 0;
+//     // // 读取数据直到遇到文件尾，计算所有读入的值的和
+//     // while( std::cin >> value ) {    //用一个 istream 对象作为条件时，其效果是检测流的状态。
+//     //                                 /*
+//     //                                 * 如果流是有效的，即流未遇到错误，那么检测成功。当遇到文件结束符(ctrl + z)，
+//     //                                 * 或遇到一个无效输入时（例如本例输入的不是整数，istream对象的装填会变为无效。
+//     //                                 * 处于无效状态的 istrem 对象会使得条件变为假。
+//     //                                 */
+//     //     sum += value;
+//     // }
+//     // std::cout << " Sum is : " << sum << std::endl;
+
+//     //1.16
+//     // int sum = 0;
+//     // int value = 0;
+//     // while(std::cin >> value) {
+//     //     sum += value;
+//     // }
+//     // std::cout << "sum is "<< sum << std::endl;
+
+//     return 0;
+// }
+
+//1.4.4
+
+
